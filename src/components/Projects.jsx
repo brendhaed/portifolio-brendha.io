87440d3cabe8b647c6c2046813d00a3f86539666
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2026-01-09 23:48:52 -0300

    adição de projeto novo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import { GoArrowUpRight } from 'react-icons/go'
 import projeto1Img from '../assets/img/projects/meu-projeto-1.png'
 import projeto2Img from '../assets/img/projects/meu-projeto-2.png'
 import projeto3Img from '../assets/img/projects/meu-projeto-3.png'
+import projeto4Img from '../assets/img/projects/meu-projeto-4.png'  
 import { FaBootstrap, FaCss3Alt, FaHtml5, FaJsSquare, FaReact } from 'react-icons/fa'
 import { SiNextdotjs, SiTailwindcss } from 'react-icons/si'
 
@@ -84,6 +85,31 @@ export function Projects(){
   </p>
   <div className="text-center mb-4">
     <a href="https://landing-page-school-tech.vercel.app/" target="_blank" className="text-purple-400 text-sm hover:underline transition">
+      Ver projeto no ar <GoArrowUpRight className="inline-block ml-1"/>
+    </a>
+  </div>
+  <div className="flex gap-3 justify-center text-purple-300 text-xl">
+    <SiNextdotjs />
+    <SiTailwindcss/>
+  </div>
+</div>
+
+  {/* Card 4  */}
+  <div className="bg-[#2b1e42] rounded-xl shadow-md p-6 max-w-sm w-full hover:shadow-[0_0_15px_4px_rgba(192,132,252,0.6)] transform transition duration-300 hover:scale-105">
+  <div className="relative mb-4">
+  <img src={projeto4Img} alt="Foto projeto 4" className="rounded w-full"/>
+  <a href="https://github.com/brendhaed/crud-series-next" target="_blank"
+     className="absolute top-2 right-2 bg-[#2b1e42]/80 w-10 h-10 flex items-center justify-center rounded-full text-purple-300 text-xl hover:text-purple-400 transition">
+    <i className="fa-brands fa-github"></i>
+  </a>
+</div>
+  <p className="text-white text-sm mb-1">Novembro 2025 - Janeiro 2026</p>
+  <h2 className="text-white text-xl font-semibold mb-2">Projeto CRUD de Séries</h2>
+  <p className="text-sm text-gray-400 mb-4">
+    Projeto que desenvolvi na Disciplina de Desenvolvimento Front-end 
+  </p>
+  <div className="text-center mb-4">
+    <a href="https://crud-series-next.vercel.app/" target="_blank" className="text-purple-400 text-sm hover:underline transition">
       Ver projeto no ar <GoArrowUpRight className="inline-block ml-1"/>
     </a>
   </div>
