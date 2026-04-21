ba22933cd61ac680552b64e9fe6b43d1a826fec8
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2026-04-20 23:28:03 -0300

    modificação do componente education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,34 +1,54 @@
 import cv from "../data/cv.json";
+
 export function Education() {
   return (
     <section
       id="educacao"
       className="bg-[#242424] rounded-2xl mx-6 md:mx-20 my-10 p-8 md:p-12 text-white"
     >
-      <h1 className="text-[#FFD67D] text-sm mb-10 text-center w-full">
-        Formação acadêmica
-      </h1>
+      {/* Título */}
+      <div className="text-center mb-12">
+        <h2 className="text-[#FFD67D] text-sm mb-2 ">
+          Formação
+        </h2>
+        <h1 className="text-2xl font-bold text-white">
+          Formação acadêmica
+        </h1>
+      </div>
 
-      <div className="flex flex-col md:flex-row items-center gap-10">
-        <div className="w-full md:w-1/2">
-          <h3 className="text-lg md:text-xl font-bold mb-2">
-            {cv.education[0].course}
-          </h3>
-          <p className="text-white text-sm font-bold mb-1">Situação</p>
-          <p className="text-white text-sm mb-4">{cv.education[0].situation}</p>
-          <p className="text-sm mb-2">{cv.education[0].institution}</p>
-          <p className="text-sm mb-4">{cv.education[0].period}</p>
-        </div>
+      {/* Grid */}
+      <div className="grid md:grid-cols-2 gap-8">
+        {cv.education.map((edu, index) => (
+          <div
+            key={index}
+            className="bg-[#1a1a1a] p-6 rounded-xl border border-transparent 
+            hover:border-purple-500/40 transition duration-300 
+            hover:shadow-[0_0_15px_rgba(168,85,247,0.3)]"
+          >
+            {/* Curso */}
+            <h3 className="text-lg font-semibold mb-2">
+              {edu.course}
+            </h3>
 
-        <div className="w-full md:w-1/2">
-          <h3 className="text-lg md:text-xl font-bold mb-2">
-            {cv.education[1].course}
-          </h3>
-          <p className="text-white text-sm font-bold mb-1">Situação</p>
-          <p className="text-white text-sm mb-4">{cv.education[1].situation}</p>
-          <p className="text-sm mb-2">{cv.education[1].institution}</p>
-          <p className="text-sm mb-4">{cv.education[1].period}</p>
-        </div>
+            {/* Instituição */}
+            <p className="text-sm text-gray-400 mb-3">
+              {edu.institution}
+            </p>
+
+            {/* Período */}
+            <p className="text-xs text-gray-500 mb-4">
+              {edu.period}
+            </p>
+
+            {/* Situação */}
+            <span
+              className="inline-block bg-purple-500/20 text-purple-300 
+              px-3 py-1 rounded-full text-xs"
+            >
+              {edu.situation}
+            </span>
+          </div>
+        ))}
       </div>
     </section>
   );
